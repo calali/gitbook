6eeb4f00e62b0cf13d9ad74116f36e5547f17789
liqiuhui <andi@xiaohongshu.com>
2021-04-25 16:56:55 +0800

    feat: add blog

--- test.js
+++ test.js
@@ -1,40 +1,87 @@
+// async function async1() {
+//   console.log('async1 start');
+//   await async2();
+//   console.log('async1 end');
+// }
+// async function async2() {
+//   console.log('async2');
+// }
+// console.log('script start');
+// setTimeout(function () {
+//   console.log('setTimeout');
+// }, 0)
+// async1();
+// new Promise(function (resolve) {
+//   console.log('promise1');
+//   resolve();
+// }).then(function () {
+//   console.log('promise2');
+// });
+// console.log('script end');
 
-function compose(...funcs) {
-  if (funcs.length === 0) {
-    return arg => arg;
-  }
 
-  if (funcs.length === 1) {
-    return funcs[0];
-  }
+// 'script start'
+// 'script end'
 
-  return funcs.reduce((a, b) => {
-    return (...args) => {
-      return a(b(...args))
-    }
-  });
+// 'async1 start'
+// 'async2'
+// 'async1 end'
+// 'promise1'
+// 'promise2'
+// 'setTimeout'
+
+// console.log('start');
+// setTimeout(() => {
+//   console.log('children2');
+//   Promise.resolve().then(() => {
+//     console.log('children3');
+//   })
+// }, 0);
+
+// new Promise(function (resolve, reject) {
+//   console.log('children4');
+//   setTimeout(function () {
+//     console.log('children5');
+//     resolve('children6')
+//   }, 0)
+// }).then((res) => {
+//   console.log('children7');
+//   setTimeout(() => {
+//     console.log(res);
+//   }, 0)
+// })
+
+// 执行整个代码块的宏任务，打印出'start'
+// 事件队列里放入34行的宏任务。
+// 执行new promise, 打印出'children4'，放入43行的宏任务
+// 执行宏任务，打印出'children2', 执行微任务，打印出'children3'
+// 执行宏任务，打印出'children5', 执行微任务，打印出'children7'
+// 执行宏任务，打印出'children6'
+
+const p = function () {
+  return new Promise((resolve, reject) => {
+    const p1 = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(1)
+      }, 0)
+      resolve(2)
+    })
+    p1.then((res) => {
+      console.log(res);
+    })
+    console.log(3);
+    resolve(4);
+  })
 }
 
-let sayHello = (...str) => `Hello , ${str.join(" And ")}`;
-let toUpper = str => str.toUpperCase();
-let replace = str => str.replace('a','replace');
-// let combin = compose(
-//   sayHello,
-//   toUpper,
-//   replace
-// );
 
-// console.log('combin', combin);
+p().then((res) => {
+  console.log(res);
+})
+console.log('end');
 
-const d = [sayHello,
-  toUpper,
-  replace].reduce((a,b)=>{
-    console.log(a,b);
-    return c = (...params)=>{
-      return a(b(...params))
-    }
-  })
-
-console.log(d);
-
-console.log(combin('alice')); //Hello , REPLACELICE
+产生一个宏任务，执行resolve(2),
+打印3，
+打印end
+执行微任务，打印出2，
+执行微任务 打印出4
